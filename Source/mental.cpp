8f257a3410b9ee5b5fe68e712c13207c8d886db0
Даниил Ермолаев <daniilermolaev@MacBook-Air-Daniil.local>
2025-12-12 22:24:34 +0500

    save

--- Source/mental.cpp
+++ Source/mental.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <vulkan/vulkan.h>
 #include <spdlog/spdlog.h>
+#include <vulkan/vulkan.h>
 
-enum MentalRendererType
-{
-    MENTAL_RENDERER_TYPE_OPENGL = 0,
-    MENTAL_RENDERER_TYPE_VULKAN = 1,
+enum MentalRendererType {
+  MENTAL_RENDERER_TYPE_OPENGL = 0,
+  MENTAL_RENDERER_TYPE_VULKAN = 1,
 };
 
-
-int main(int argc, const char* argv[])
-{
-    spdlog::info("Hello, World!");
-    return 0;
+int main(int argc, const char* argv[]) {
+  spdlog::info("Hello, World!");
+  return 0;
 }
